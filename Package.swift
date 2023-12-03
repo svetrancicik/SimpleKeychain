eff76a2ecb43daff9c81823870edc4fd1ca536da
Svetozar Rancic <svetozar.rancic@ingka.ikea.com>
2023-12-03 22:43:41 +0100

    xrOS support

--- Package.swift
+++ Package.swift
@@ -1,14 +1,31 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SimpleKeychain",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
-    products: [.library(name: "SimpleKeychain", targets: ["SimpleKeychain"])],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v7),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: "SimpleKeychain",
+            targets: ["SimpleKeychain"]
+        )
+    ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "7.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0")),
+        .package(
+            url: "https://github.com/svetrancicik/Quick.git",
+            branch: "quick-7-3-0"
+        ),
+        .package(
+            url: "https://github.com/svetrancicik/Nimble.git",
+            branch: "nimble-13-0-0"
+        )
     ],
     targets: [
         .target(
